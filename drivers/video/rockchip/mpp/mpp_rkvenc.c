b394700ee7806286afd4fc3c9a5386f044f411d4
jjm2473 <jjm2473@gmail.com>
2025-04-02 13:03:51 +0800

    mpp: ignore devfreq init failed

--- drivers/video/rockchip/mpp/mpp_rkvenc.c
+++ drivers/video/rockchip/mpp/mpp_rkvenc.c
@@ -1174,8 +1174,14 @@ static int rkvenc_init(struct mpp_dev *mpp)
 
 #ifdef CONFIG_PM_DEVFREQ
 	ret = rkvenc_devfreq_init(mpp);
-	if (ret)
-		mpp_err("failed to add venc devfreq\n");
+	if (ret) {
+		mpp_err("failed to add venc devfreq: %d\n", ret);
+		if (-EPROBE_DEFER == ret) {
+			return ret;
+		}
+		mpp_err("init venc without devfreq\n");
+		ret = 0;
+	}
 #endif
 
 	/* for mmu pagefault */
