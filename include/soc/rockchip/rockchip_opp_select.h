b53308643ce75bd4b1ffb47572d19c8c2aed8529
jjm2473 <jjm2473@gmail.com>
2025-09-03 15:05:32 +0800

    include/soc/rockchip: sync from develop-6.6

--- include/soc/rockchip/rockchip_opp_select.h
+++ include/soc/rockchip/rockchip_opp_select.h
@@ -44,6 +44,7 @@ struct volt_rm_table {
 struct rockchip_opp_data {
 	config_clks_t config_clks;
 	config_regulators_t config_regulators;
+	bool is_use_pvtpll;
 
 	int (*get_soc_info)(struct device *dev, struct device_node *np,
 			    int *bin, int *process);
@@ -97,6 +98,7 @@ struct pvtpll_opp_table {
  * @init_freq:		Set the initial frequency when init opp table.
  * @is_rate_volt_checked: Marks if device has checked initial rate and voltage.
  * @pvtpll_clk_id:      Device's clock id.
+ * @pvtpll_smc:		Marks if smc call of pvtpll is available.
  * @pvtpll_low_temp:    Marks if device has low temperature pvtpll config.
  */
 struct rockchip_opp_info {
@@ -138,8 +140,14 @@ struct rockchip_opp_info {
 	bool is_rate_volt_checked;
 
 	u32 pvtpll_clk_id;
+	bool pvtpll_smc;
 	bool pvtpll_low_temp;
 };
+
+static inline bool rockchip_opp_is_use_pvtpll(struct rockchip_opp_info *info)
+{
+	return (info->is_scmi_clk || (info->data && info->data->is_use_pvtpll));
+}
 
 #if IS_ENABLED(CONFIG_ROCKCHIP_OPP)
 int rockchip_of_get_leakage(struct device *dev, char *lkg_name, int *leakage);
@@ -166,6 +174,8 @@ int rockchip_set_intermediate_rate(struct device *dev,
 				   struct clk *clk, unsigned long old_freq,
 				   unsigned long new_freq, bool is_scaling_up,
 				   bool is_set_clk);
+int rockchip_opp_set_low_length(struct device *dev, struct device_node *np,
+				struct rockchip_opp_info *opp_info);
 int rockchip_opp_config_regulators(struct device *dev,
 				     struct dev_pm_opp *old_opp,
 				     struct dev_pm_opp *new_opp,
@@ -249,6 +259,13 @@ rockchip_set_intermediate_rate(struct device *dev,
 			       struct clk *clk, unsigned long old_freq,
 			       unsigned long new_freq, bool is_scaling_up,
 			       bool is_set_clk)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int
+rockchip_opp_set_low_length(struct device *dev, struct device_node *np,
+			    struct rockchip_opp_info *opp_info)
 {
 	return -EOPNOTSUPP;
 }
